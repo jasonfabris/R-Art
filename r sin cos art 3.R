06a576a4366c2ec133bb86fb816e0836c333a79c
Jason Fabris <jay.fabris@rogers.com>
2017-07-03 21:28:33 -0400

    adjust r s-c-a 3
    adj

--- r sin cos art 3.R
+++ r sin cos art 3.R
@@ -2,7 +2,7 @@
 
 set.seed(79)
 
-par(mfrow=c(1,4), ann=FALSE, pty='s', cex=0.2, mai=c(.05,.05,.05,.05), mar=c(0.1, 0.1, 0.1, 0.1), oma=c(0,0,0,0))
+par(mfrow=c(2,4), ann=FALSE, pty='s', cex=0.2, mai=c(.05,.05,.05,.05), mar=c(0.1, 0.1, 0.1, 0.1), oma=c(0,0,0,0))
 
 x <- seq(-1300,1300, by=0.015)
 
@@ -22,9 +22,16 @@ d <- b + rnorm(length(b), mean=15, sd=5)
 e <- b + rnorm(length(b), mean=5, sd=6)
 
 plot(cos(e/x), sin(d/x), axes=FALSE, col=rgb(20, 32, 75, 45, max=255))
+plot(c,d, axes=FALSE, col=rgb(20, 82, 75, 25, max=255))
 plot(sin(d%%x), cos(d/x), axes=FALSE, col=rgb(20, 32, 75, 45, max=255))
-plot(sin(c%%abs(x)), cos(d/x), axes=FALSE, col=rgb(20, 22, 95, 85, max=255))
-plot(sin(e/x), cos(d/x), axes=FALSE, col=rgb(20, 32, 75, 45, max=255))
+plot(sin(c%%abs(x)), cos(d/x), axes=FALSE, col=rgb(20, 72, 95, 25, max=255))
+
+plot(d,c, axes=FALSE, col=rgb(20, 82, 75, 25, max=255))
+plot(sin(x/e), cos(d/x), axes=FALSE, col=rgb(20, 32, 75, 45, max=255))
+plot(sin(e/x), cos(d/x), axes=FALSE, col=rgb(15, 52, 85, 65, max=255))
+plot(sin(x/e), sqrt(acos((x%%c)/d)), axes=FALSE, col=rgb(20, 32, 75, 25, max=255))
+
+
 
 # plot(sin(c*(log(x)*3)), cos(d*x), axes=FALSE, col=rgb(0, 125, 255, 75, max=255))
 # plot(sin(a/x), cos(b*x), axes=FALSE, col=rgb(50, 100, 10, 75, max=255))
